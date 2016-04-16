ae735450d8678883486683618eba37557ef7bcd8
Primaël Quémerais <primaelq@gmail.com>
2016-04-16 12:10:38 +0100

    PlayerController

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float sensitivity = 5.0f;
+    public float xClamp = 60.0f;
+
+    private Vector3 movement;
+
+    private float xRotation = 0.0f;
+
+	void Start ()
+    {
+        Cursor.visible = false;
+	}
+	
+	void Update ()
+    {
+        float yRotation = Input.GetAxis("Mouse X") * sensitivity;
+
+        transform.Rotate(0.0f, yRotation, 0.0f);
+
+        xRotation -= Input.GetAxis("Mouse Y") * sensitivity;
+        xRotation = Mathf.Clamp(xRotation, -xClamp, xClamp);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
+
+        movement.z = Input.GetAxis("Vertical") * moveSpeed;
+        movement.x = Input.GetAxis("Horizontal") * moveSpeed;
+        movement.y = 0.0f;
+
+        movement = transform.rotation * movement;
+
+        CharacterController controller = GetComponent<CharacterController>();
+
+        controller.SimpleMove(movement);
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+            Cursor.visible = true;
+    }
+}
